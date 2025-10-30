4d41e301472a3fc401e2a0258873ce72c4563a21
ltostes <lucasrmtostes@gmail.com>
2025-10-30 09:40:31 -0300

    added redirect for rss to rss.xml

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
       '/*': ['./content/**/*'],
     },
   },
+  async redirects() {
+    return [
+      {
+        source: '/rss',
+        destination: '/rss.xml',
+        permanent: true, // Use true for 308 (permanent) or false for 307 (temporary)
+      },
+    ];
+  },
 };
